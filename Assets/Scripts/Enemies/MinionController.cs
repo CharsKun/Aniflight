de4b6d0be32b541b8055f34586da3fefd8237b1f
Lumphyrux <lumphyrux@gmail.com>
2020-11-29 16:34:01 +0700

    Update 0.9
    Enemy : Goblin Added + Goblin2 'Variant' Added
    Script : EnemyController Added
    (EnemyController used to control enemy movement following path and waves)
    ScriptGameTool : StageController Added
    (StageController used to make waves of minions and their waypoints/movement)

---
+++ Assets/Scripts/Enemies/MinionController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionController : MonoBehaviour
+{   
+    private float moveSpeed = 3f;
+
+    public int pathIndex;
+    private int pathPointIndex;
+
+    public float spawnTime;
+
+    //public float speed = 3f;
+    private Rigidbody2D rb;
+    private int Health = 5;
+    private int RandomVariable;
+
+    public GameObject explode;
+    private Renderer rend;
+    private float redTime = 0f;
+    //redTime = waktu berapa lama warna dioverlay merah
+
+    public GameObject PowerUp;
+    private GameObject puspawn;
+    public GameObject PowerUpMana;
+    private GameObject puspawn2;
+    public GameObject PowerUpHealth;
+    private GameObject puspawn3;
+
+    //public GameObject ManagerGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = Time.timeSinceLevelLoad;
+        rend = this.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        redTime -= 1 * Time.deltaTime;
+        if (redTime <= 0f)
+        {
+            redTime = 0f;
+            rend.material.color = Color.white;
+        }
+
+        if (Health <= 0)
+        {
+            if (transform.position.y <= -5.5f)
+            {
+                RandomVariable = Random.Range(0, 10);
+                if (RandomVariable == 4)
+                {
+                    puspawn = Instantiate(PowerUp);
+                    puspawn.transform.position = transform.position;
+                    puspawn.transform.rotation = transform.rotation;
+                    puspawn.SetActive(true);
+                }
+                else if (RandomVariable == 6)
+                {
+                    puspawn2 = Instantiate(PowerUpMana);
+                    puspawn2.transform.position = transform.position;
+                    puspawn2.transform.rotation = transform.rotation;
+                    puspawn2.SetActive(true);
+                }
+                else if (RandomVariable == 8)
+                {
+                    puspawn3 = Instantiate(PowerUpHealth);
+                    puspawn3.transform.position = transform.position;
+                    puspawn3.transform.rotation = transform.rotation;
+                    puspawn3.SetActive(true);
+                }
+            }
+            //ManagerGame.GetComponent<Manager>().count++;
+            Instantiate(explode, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+
+
+
+        var path = StageController.current.GetPath(pathIndex);
+        if (path == null)
+        {
+            return;
+        }
+        var target = path[pathPointIndex];
+        var delta = target - (Vector2)transform.position;
+      
+        transform.position = Vector2.MoveTowards(transform.position,target, moveSpeed*Time.deltaTime);
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            pathPointIndex++;
+        }
+
+        if (pathPointIndex >= path.Count)
+            {
+                Destroy(this.gameObject);
+            }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "BulletPlayer")
+        {
+            Health--;
+            //Debug.Log("Health : "+ Health);
+            rend.material.color = Color.red;
+            redTime = 0.05f;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/GameTool/StageController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+using System.Linq;
+
+public class StageController : MonoBehaviour
+{
+    public static StageController current;
+    public Transform[] waypointGroups;
+
+    private List<List<Vector2>> paths;
+    private int pathCount;
+
+    [Serializable]
+    public struct MinionSpawnData
+    {
+        public GameObject minion;
+        public float delay;
+        public int amount;
+        public float interval;
+        public int waypointIndex;
+    }
+
+    [Serializable]
+    public struct WaveData
+    {
+        public MinionSpawnData[] spawns;
+        public float time;
+    }
+
+    public int WaveIndex { get; private set; }
+    private float currentWaveTime;
+
+    private float respawnTime = 1f;  
+    private int count = 0 ;
+    private float time;
+
+    public float WaveProgress { get; private set; }
+
+    public WaveData[] waves;
+
+    private int spawnCount;
+    private float spawnTimer;
+
+    public StageController()
+    {
+        current = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        //StartCoroutine(startWave());
+
+        paths = new List<List<Vector2>>();
+
+        foreach (var group in waypointGroups)
+        {
+            var path = new List<Vector2>();
+
+            path.Add(group.position);
+            int childCount = group.childCount;
+            for (int i = 0; i < childCount; ++i)
+            {
+                var child = group.GetChild(i);
+                path.Add(child.position);
+            }
+
+            paths.Add(path);
+        }
+        pathCount = paths.Count;
+    }
+
+    public List<Vector2> GetPath(int index)
+    {
+        if (index < 0 || index >= pathCount) return null;
+        return paths[index];
+    }
+
+    private int GetWaveSpawnIndex(float time, MinionSpawnData data)
+    {
+        return (int)((time - data.delay)/data.interval);
+    }
+
+    public float SecondsToBeats(float seconds)
+    {
+        return seconds * 120f / 60;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {  
+        time += Time.deltaTime;
+        currentWaveTime +=  SecondsToBeats(Time.deltaTime);
+        
+        if (WaveIndex < waves.Length)
+        {
+            var wave = waves[WaveIndex];
+            float spawnEndTime = 0;
+            
+            foreach (var spawnData in wave.spawns)
+            {
+                spawnEndTime = Mathf.Max(spawnEndTime, spawnData.delay + (spawnData.interval * (spawnData.amount - 1)));
+                spawnTimer += Time.deltaTime;
+                if (spawnTimer-spawnData.delay>spawnData.interval && spawnCount < spawnData.amount)
+                {
+                    var e = Instantiate(spawnData.minion, waypointGroups[spawnData.waypointIndex].transform.position, Quaternion.identity);
+                    e.GetComponent<MinionController>().pathIndex = spawnData.waypointIndex;
+                    spawnCount++;
+                    spawnTimer = 0;
+                }
+            }
+
+            bool hasEnemy = FindObjectOfType<MinionController>() != null;
+            
+            if (WaveIndex < waves.Length - 1)
+            {
+                if (currentWaveTime >= spawnEndTime && !hasEnemy && currentWaveTime < waves[WaveIndex].time)
+                {
+                    currentWaveTime = waves[WaveIndex].time;
+                }
+
+                if (currentWaveTime >= waves[WaveIndex].time + 10f)
+                {
+                    WaveIndex++;
+                    currentWaveTime = 0;
+                    spawnCount = 0;
+                }
+
+                if (currentWaveTime <= waves[WaveIndex].time)
+                {
+                    WaveProgress = currentWaveTime / waves[WaveIndex].time;
+                }
+                else
+                {
+                    WaveProgress = 1f + ((currentWaveTime - waves[WaveIndex].time) / 10f);
+                }
+            }
+            else
+            {
+                WaveProgress = currentWaveTime / spawnEndTime;
+                if (WaveProgress > 1f)
+                {
+                    WaveProgress = 1f;
+                    
+                }
+
+                /*if (currentWaveTime >= spawnEndTime && !hasEnemy)
+                {
+                    WaveIndex++;
+                    currentWaveTime = 0;
+                    spawnCount = 0;
+                }*/
+            }
+        }
+    }
+}
